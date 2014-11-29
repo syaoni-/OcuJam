c651d96e89432d8f8e29366287ecf4b00bac3e6f
syaony <e125752@ie.u-ryukyu.ac.jp>
2014-11-29 13:22:40 +0900

    SingletonManager,InputManagerの追加

---
+++ OcuJamProject/Assets/Users/Uehara/Scripts/Manager/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : SingletonMonoBehaviour<InputManager> {
+
+	public void Awake()
+	{
+		if(this != Instance)
+		{
+			Destroy(this);
+			return;
+		}
+		
+		DontDestroyOnLoad(this.gameObject);
+	}    
+
+	public static float h;
+	public static float v;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Select"))
+				Debug.Log("hoge");
+	}
+}
